f864da3b529435febbb871f269e7faa57f26e152
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2023-03-03 09:33:07 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectIsLoggedIn } from 'redux/selectorsUser';
 
 import s from './HomePage.module.css';
 
 function HomePage() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div className={s.container}>
       <div className={s.box}>
@@ -12,10 +17,13 @@ function HomePage() {
         </div>
         <h1 className={s.text}> PhoneBook</h1>
       </div>
-      <h2 className={s.infotext}>
-        To start using <a href="/goit-react-hw-08-phonebook/sign-in">Sign in</a>{' '}
-        or <a href="/goit-react-hw-08-phonebook/sign-up">Sign Up</a>
-      </h2>
+      {isLoggedIn ? null : (
+        <h2 className={s.infotext}>
+          To start using{' '}
+          <a href="/goit-react-hw-08-phonebook/sign-in">Sign in</a> or{' '}
+          <a href="/goit-react-hw-08-phonebook/sign-up">Sign Up</a>
+        </h2>
+      )}
     </div>
   );
 }
